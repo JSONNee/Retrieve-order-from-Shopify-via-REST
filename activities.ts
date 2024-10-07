a0b5cbcc4277ddc32c421d6693ff574d824a3094
JSONNee <nam.le@vietnamtechlab.com>
2024-10-07 12:36:37 +1000

    Confirm

--- activities.ts
+++ activities.ts
@@ -9,7 +9,7 @@ const headers = {
   'X-Shopify-Access-Token': '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'
 };
 export async function getOrder(id: number): Promise<any> {
-        const response = await axios.get(`${url}/${id}.json`, { headers });
+        const response = await axios.get(`${url}/products/${id}.json`, { headers });
         const data = response.data;
         console.log(data);
         return data;
