921e77a0af2b8ec39e121d34c1b52fc18cef4d1f
JSONNee <nam.le@vietnamtechlab.com>
2024-10-22 10:32:43 +0700

    Update activities.ts

--- activities.ts
+++ activities.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'; 
 
-const shopifyAdmin = 'vtlclothingstore.myshopify.com';
+const shopifyAdmin = 'vtldeveloper.myshopify.com';
 const shopifyBooby = '2023-07';
 const url = `https://${shopifyAdmin}/admin/api/${shopifyBooby}`;
-const accessToken = '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';
+const accessToken = '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';
 const headers = {
   'Content-Type': 'application/json',
-  'X-Shopify-Access-Token': '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'
+  'X-Shopify-Access-Token': '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'
 };
 export async function getOrder(id: number): Promise<any> {
         const response = await axios.get(`${url}/products/${id}.json`, { headers });
